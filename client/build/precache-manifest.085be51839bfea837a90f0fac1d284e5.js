89517bc02e245f002948d72698e3b422deff6ec9
mdarsie <github@darsie.me>
2020-01-18 19:38:42 -0500

    npm install

---
+++ client/build/precache-manifest.085be51839bfea837a90f0fac1d284e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b4f753d2cfcfed3ee84988e04866f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76bed4a5f2219ff297eb",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "76bed4a5f2219ff297eb",
+    "url": "/static/js/main.5c9cf1ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
